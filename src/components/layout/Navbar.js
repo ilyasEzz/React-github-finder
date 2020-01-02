eb8641be5e54052494edb7ba22cc6ca7207ec3d5
Ilyas Ezzahid <ezzil1997@gmail.com>
2020-01-03 00:11:31 +0700

    Navbar component

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class Navbar extends Component {
+    static defaultProps = {
+        title: "Github Finder",
+        icon: "fab fa-github"
+
+    }
+
+    static propTypes = {
+        title: PropTypes.string.isRequired,
+        icon: PropTypes.string
+
+    }
+
+    render() {
+        return (
+            <nav className="navbar navbar-bg-dark bg-success">
+                <a href="#" className="navbar-brand text-white">
+                    <i className={this.props.icon}></i> {this.props.title}
+                </a>
+            </nav>
+        )
+    }
+}
+
+export default Navbar
