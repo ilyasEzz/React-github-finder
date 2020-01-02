edb88b7144f26345875feaa9fc286517d69342fd
Ilyas Ezzahid <ezzil1997@gmail.com>
2020-01-02 23:05:51 +0700

    restarting the project from scatch

--- src/App.js
+++ src/App.js
@@ -1,117 +1,26 @@
-import React, { Fragment, useState } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import axios from "axios";
-import "./App.css";
-import Navbar from "./components/Layout/Navbar";
-import Users from "./components/users/Users";
-import User from "./components/users/User";
-import Search from "./components/users/Search";
-import Alert from "./components/Layout/Alert";
-import About from "./components/pages/About";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-  // Set the State
-  const [users, setUsers] = useState([]);
-  const [user, setUser] = useState({});
-  const [loading, setLoading] = useState(false);
-  const [alert, setAlert] = useState(null);
-  const [repos, setRepos] = useState([]);
-
-  // Search Github Users
-  const searchUsers = async text => {
-    // showing the loading gif while fetching
-    setLoading(true);
-
-    // fetching the github users correspending to to search
-    const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${
-        process.env.REACT_APP_GITHUB_CLIENT_ID
-      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-    // updating the user state with all the fetched data then removing the loading gif
-    setUsers(res.data.items);
-    setLoading(false);
-  };
-
-  // get resent repositories
-  const getUserRepos = async login => {
-    setLoading(true);
-
-    const res = await axios.get(
-      `https://api.github.com/users/${login}/repos?per_page=5&sort=created:asc&client_id=${
-        process.env.REACT_APP_GITHUB_CLIENT_ID
-      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-
-    setRepos(res.data);
-    setLoading(false);
-  };
-
-  // Single Github User
-  const getUser = async login => {
-    setLoading(true);
-
-    const res = await axios.get(
-      `https://api.github.com/users/${login}?client_id=${
-        process.env.REACT_APP_GITHUB_CLIENT_ID
-      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-
-    setUser(res.data);
-    setLoading(false);
-  };
-
-  // Clear users from state
-  const clearUsers = () => setUsers([]);
-
-  // Alert
-  const showAlert = (msg, type) => {
-    setAlert({ msg, type });
-    setTimeout(() => setAlert({}), 3000);
-  };
-
+function App() {
   return (
-    <Router>
-      <div className="App ">
-        <Navbar />
-        <div className="container">
-          <Alert alert={alert} />
-          <Switch>
-            <Route
-              exact
-              path="/"
-              render={props => (
-                <Fragment>
-                  <Search
-                    searchUsers={searchUsers}
-                    clearUsers={clearUsers}
-                    showClear={users.length > 0 ? true : false}
-                    setAlert={showAlert}
-                  />
-                  <Users users={users} loading={loading} />
-                </Fragment>
-              )}
-            />
-            <Route exact path="/about" component={About} />
-            <Route
-              exact
-              path="/user/:login"
-              render={props => (
-                <User
-                  {...props}
-                  getUser={getUser}
-                  getUserRepos={getUserRepos}
-                  repos={repos}
-                  user={user}
-                  loading={loading}
-                />
-              )}
-            />
-          </Switch>
-        </div>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
