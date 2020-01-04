15eedc9166ec5a105e9b796248a09a7f3daacec0
Ilyas Ezzahid <ezzil1997@gmail.com>
2020-01-04 20:48:02 +0700

    refactoring the Navbar and UserItem to functional component

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-export class Navbar extends Component {
-    static defaultProps = {
-        title: "Github Finder",
-        icon: "fab fa-github"
-
-    }
-
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        icon: PropTypes.string
-
-    }
-
-    render() {
-        return (
-            <nav className="navbar navbar-bg-dark bg-success mb-4">
-                <a href="#" className="navbar-brand text-white">
-                    <i className={this.props.icon}></i> {this.props.title}
-                </a>
-            </nav>
-        )
-    }
+const Navbar = ({ icon, title }) => {
+    return (
+        <nav className="navbar navbar-bg-dark bg-success mb-4">
+            <a href="#" className="navbar-brand text-white">
+                <i className={icon}></i> {title}
+            </a>
+        </nav>
+    )
 }
+
+Navbar.defaultProps = {
+    title: "Github Finder",
+    icon: "fab fa-github"
+
+}
+
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string
+
+}
+
 
 export default Navbar
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,27 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react';
 
-class UserItem extends Component {
-    state = {
-        login: "mojombo",
-        id: 1,
-        avatar: "https://avatars0.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo",
-
-    }
-    render() {
-        const { login, avatar, html_url, id } = this.props.user
-        return (
-            <div className="card text-center">
-                <div className="card-img-top">
-                    <img src={avatar} alt="avatar" className="rounded-circle" width="60px" />
-                </div>
-                <div className="card-body">
-                    <p>{login}</p>
-                    <a href={html_url} target="blank" className="btn btn-dark">Learn More</a>
-                </div>
+const UserItem = ({ user: { login, avatar, html_url } }) => {
+    return (
+        <div className="card text-center">
+            <div className="card-img-top">
+                <img src={avatar} alt="avatar" className="rounded-circle" width="60px" />
             </div>
-        )
-    }
+            <div className="card-body">
+                <p>{login}</p>
+                <a href={html_url} target="blank" className="btn btn-dark">Learn More</a>
+            </div>
+        </div>
+    )
 }
 
 export default UserItem
